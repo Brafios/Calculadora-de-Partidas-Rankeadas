0b2f2b66e8160a27cb16346c51be9dd53c764392
Brafios <44183729+Brafios@users.noreply.github.com>
2024-12-05 11:44:32 -0300

    Create TerceiroMetodo.js

---
+++ TerceiroMetodo.js
@@ -0,0 +1,58 @@
+   
+function balance()
+{
+	let cont = 0
+	let numberVictories = 0
+	for (cont = 0; cont < 90; cont++)
+	{
+		numberVictories++
+	}
+    
+	let numberDefeats = 0
+	for (cont = 0; cont < 10; cont++)
+	{
+        numberDefeats++
+	}
+    
+	let balance = numberVictories - numberDefeats
+    
+	if (balance <= 10)
+	{
+		nivel = "ferro"
+	}
+
+	else if (balance <= 20)
+	{
+		nivel = "bronze"
+	}
+
+	else if (balance <= 50)
+	{
+		nivel = "prata"
+	}
+
+	else if (balance <= 80)
+	{
+		nivel = "ouro"
+	}
+
+	else if (balance <= 90)
+	{
+		nivel = "diamante"
+	}
+
+	else if (balance <= 100)
+	{
+		nivel = "lendário"
+	}
+
+	else 
+	{
+		nivel = "imortal"
+	}
+    
+    console.log("O Héroi tem saldo de vitórias de: " + balance + " está no nivel: " + nivel)
+    
+}
+
+balance()
