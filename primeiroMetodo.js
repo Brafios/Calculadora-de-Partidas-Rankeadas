bc475b75ec85a3b17d560425057674a2c8e47c0f
Brafios <44183729+Brafios@users.noreply.github.com>
2024-12-05 11:10:15 -0300

    Create primeiroMetodo.js

---
+++ primeiroMetodo.js
@@ -0,0 +1,61 @@
+function numberVictories()
+{
+	let cont = 0
+	let numberVictories = 0
+	for (cont = 0; cont < 100; cont++)
+    {
+        numberVictories++
+    }
+    return numberVictories
+}
+
+function numberDefeats()
+{
+	let cont = 0
+	let numberDefeats = 0
+	for (cont = 0; cont < 10; cont++)
+    {
+        numberDefeats++
+    }
+    return numberDefeats
+}
+
+let balance = numberVictories() - numberDefeats()
+let nivel = " "
+
+if (balance < 10)
+{
+	nivel = "ferro"
+}
+
+else if (balance < 20)
+{
+	nivel = "bronze"
+}
+
+else if (balance < 50)
+{
+	nivel = "prata"
+}
+
+else if (balance < 80)
+{
+	nivel = "ouro"
+}
+
+else if (balance <= 90)
+{
+	nivel = "diamante"
+}
+
+else if (balance < 100)
+{
+	nivel = "lendário"
+}
+
+else 
+{
+	nivel = "imortal"
+}
+
+console.log("O Héroi tem saldo de vitórias de: " + balance + " está no nivel: " + nivel)
